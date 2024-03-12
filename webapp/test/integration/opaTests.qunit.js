351675bae04059d367f5c1d13fbfe9795b9dafd1
Gulcan COSKUN <gulcan.coskun@alliance4u.fr>
2024-03-12 10:26:42 +0100

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["gcosui5calculator/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"gcos_ui5_calculator/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
