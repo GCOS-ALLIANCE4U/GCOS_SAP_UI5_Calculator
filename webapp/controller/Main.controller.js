ac91793bd447ab20e34420d2bd005f8c91670394
Gulcan COSKUN <gulcan.coskun@alliance4u.fr>
2024-03-12 13:44:16 +0100

    create calculate function and use swith case to create calculator

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -10,6 +10,33 @@ sap.ui.define([
         return Controller.extend("gcosui5calculator.controller.Main", {
             onInit: function () {
 
+            },
+            calculate: function() {
+               var operant1 = this.byId("operant1"),
+                operant2 = this.byId("operant2"),
+                operator = this.byId("operator"),
+
+                // we have string type, to convert it to integer use parseInt
+                sValue1 = parseInt(operant1.getValue(),10),
+                sValue2 = parseInt(operant2.getValue(),10),
+                sOperator = operator.getSelectedKey(),
+                iResult = 0;
+
+                switch(sOperator) {
+                    case '+':
+                        iResult = sValue1 + sValue2;
+                        break;
+                    case '-':
+                        iResult = sValue1 - sValue2;
+                        break;
+                    case '*':
+                        iResult = sValue1 * sValue2;
+                        break;
+                    case '/':
+                        iResult = sValue1 / sValue2;
+                        break;
+                }
+                alert("The result:" + iResult);
             }
         });
     });
