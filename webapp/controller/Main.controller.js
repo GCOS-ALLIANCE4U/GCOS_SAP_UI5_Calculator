8d89e7bee550222855d88d13cb6ed7a3355718ca
Gulcan COSKUN <gulcan.coskun@alliance4u.fr>
2024-03-13 11:18:57 +0100

    add a result message by using getResourceBundle

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,52 +1,53 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     // import JSON model library
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, JSONModel) {
+    function (Controller, JSONModel, ResourceModel) {
         "use strict";
 
         return Controller.extend("gcosui5calculator.controller.Main", {
             onInit: function () {
                 var oCalcData = {
                     // we will pass all variables to the main xml
-                    op1:'1',
-                    op2:'2',
-                    operator: '',
+                    op1: '1',
+                    op2: '2',
+                    operator: '+',
                     result: '',
                     // we will pass this variable to the items attirbute in select
                     aSelect: [
-                        {key: '+'},
-                        {key: '-'},
-                        {key: '*'},
-                        {key: '/'}
+                        { key: '+' },
+                        { key: '-' },
+                        { key: '*' },
+                        { key: '/' }
                     ]
                 },
-                oModel = new JSONModel(oCalcData);
+                    oModel = new JSONModel(oCalcData);
                 this.getView().setModel(oModel);
             },
-            calculate: function() {
-               var oModel = this.getView().getModel(),
-            /*  operant1 = this.byId("operant1"),
-                operant2 = this.byId("operant2"),
-                operator = this.byId("operator"), */
+            calculate: function () {
+                var oModel = this.getView().getModel(),
+                    /*  operant1 = this.byId("operant1"),
+                        operant2 = this.byId("operant2"),
+                        operator = this.byId("operator"), */
 
-                // we have string type, to convert it to integer use parseInt
-            /*  sValue1 = parseInt(operant1.getValue(),10),
-                sValue2 = parseInt(operant2.getValue(),10),
-                sOperator = operator.getSelectedKey(), */
+                    // we have string type, to convert it to integer use parseInt
+                    /*  sValue1 = parseInt(operant1.getValue(),10),
+                        sValue2 = parseInt(operant2.getValue(),10),
+                        sOperator = operator.getSelectedKey(), */
 
-                sValue1 = parseInt(oModel.getProperty("/op1"),10),
-                sValue2 = parseInt(oModel.getProperty("/op2"),10),
-                sOperator = oModel.getProperty("/operator"),
+                    sValue1 = parseInt(oModel.getProperty("/op1"), 10),
+                    sValue2 = parseInt(oModel.getProperty("/op2"), 10),
+                    sOperator = oModel.getProperty("/operator"),
 
-                iResult = 0;
+                    iResult = 0;
 
-                switch(sOperator) {
+                switch (sOperator) {
                     case '+':
                         iResult = sValue1 + sValue2;
                         break;
@@ -60,8 +61,19 @@ sap.ui.define([
                         iResult = sValue1 / sValue2;
                         break;
                 }
-           // alert("The result:" + iResult); 
-           oModel.setProperty("/result1", iResult);
+               // alert("The result:" + iResult);
+                oModel.setProperty("/result", iResult);
+            },
+
+            formatResult: function (iResult) {
+                var oBundle = this.getView().getModel("i18n") && this.getView().getModel("i18n").getResourceBundle();
+                if (oBundle) {
+                    var sResult = oBundle.getText("appResult", [iResult]);
+                    if (iResult) {
+                        return sResult;
+                    }
+                }
+                return "";
             }
         });
     });
